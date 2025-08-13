adf3c36b44504a0b8524d6ce5fe5e807e77bd90d
josue <josuejuniorww@gmail.com>
2025-08-13 16:12:53 -0300

    build: :construction_worker: ziggy no vite config

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': '/resources/js',
+            'ziggy-js': '/vendor/tightenco/ziggy/dist/index.esm.js',
         },
     },
 });
